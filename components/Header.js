1d022d5d97bebfb6d283a66ad2ba0719505ac3ef
damienbiabiany <damienbiabiany@yahoo.fr>
2022-04-13 19:06:13 +0200

    building the left side of the header

--- components/Header.js
+++ components/Header.js
@@ -19,22 +19,25 @@ import {
 function Header() {
   return (
     <div>
-        <h1>Header</h1>
         {/* Left */}
-        <div className='flex items-center'>
+        <div className="flex items-center">
             {/* Image Component does lazy loading */}
             <Image  src={mypic}
                     width={40}
                     height={40}
                     layout="fixed"
                     alt=""/>
-        </div>
-        <div className='flex'>
-            <SearchIcon className='h-6'/>
-            <input type="text" placeholder='Search Facebook'/>
+            <div className="flex items-center p-2 ml-2 bg-gray-100 rounded-full">
+                <SearchIcon className="h-6 text-gray-600"/>
+            <input className="flex items-center ml-2 placeholder-gray-500 bg-transparent outline-none"
+                   type="text"
+                   placeholder='Search Facebook'/>
+            </div>
         </div>
         {/* Center */}
-
+       <div className="flex justify-center flex-grow">
+           <div></div>
+       </div>
         {/* Right */}
     </div>
   )
